bc4e31b8690a875e80404b58cd7e9b517a6e0ed3
pedroski01 <cuentagib123@gmail.com>
2023-07-17 19:39:35 +0100

    p

--- PERL/actividad03.js
+++ PERL/actividad03.js
@@ -3,21 +3,16 @@ var x =parseInt(prompt("introduce el primer número"));
 console.log("introduce el segundo número");
 var y=parseInt(prompt("introduce el segundo número"));
 if(x>=y){
-    var z=true ;
-
-    }
-    else if(x==y){
+    if(x==y){
         console.log("son iguales");
-    }
-    else{
-        console.log(x+"es el mayor de los dos")
-        
-    }
-    
-    if((x===y)=false){
-        console.log(y+"es el mayor");
-    }
 
-    else{
-        console.log(x+"es el mayor de los dos")
     }
+    else {
+        console.log(x+"ES MAYOR");
+    }
+}
+else{
+        console.log(y+"es el mayor de los dos")
+        
+}
+    
